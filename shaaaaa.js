36807ce15daecbf3886779043e965bc21a9a3ecc
Netsville Developer <netsville@devel4.netsville.com>
2016-01-01 18:07:05 -0500

    Success!
    tls gives us the certificate in DER format.  Figured out how to convert
    to PEM by hand so we only need x509 to extract signature algorithm.
    
    No more OpenSSL!

--- shaaaaa.js
+++ shaaaaa.js
@@ -10,8 +10,8 @@ var fs = require('fs'); // loads root certs
 // yorkie's fork, includes signatureAlgorithm
 var x509 = require("x509");
 
+// network cxn handled directly by tls
 var tls = require('tls');
-var openssl = require('openssl-wrapper');
 
 var Shaaa = {
 
@@ -71,14 +71,11 @@ var Shaaa = {
     return {algorithm: answer, raw: raw, good: good};
   },
 
-  // using openssl to convert DER encoded cert to PEM format
-  derToPem: function(derBuffer, callback) {
-    openssl.exec('x509', derBuffer, { inform: 'der', outform: 'pem' }, function(error, pemBuffer) {
-      if (error)
-        callback(error);
-      else
-        callback(null, pemBuffer);
-    });
+  // Convert DER certificate to PEM
+  derToPem: function(derBuffer) {
+    var b64Der = derBuffer.toString('base64');
+    var b64DerLines = b64Der.match(/.{1,64}/g);
+    return "-----BEGIN CERTIFICATE-----\n" + b64DerLines.join('\n') + "\n-----END CERTIFICATE-----\n";
   },
 
   certs: function(domain, callback, options) {
@@ -110,65 +107,6 @@ var Shaaa = {
       rejectUnauthorized: false
     };
 
-/*
-[tlsSocket] connected
-{ subject: 
-   { C: 'US',
-     ST: 'NY',
-     L: 'Rochester',
-     O: 'Netsville, Inc.',
-     OU: 'Wildcard Division',
-     CN: '*.netsville.com' },
-  issuer: 
-   { C: 'US',
-     O: 'DigiCert Inc',
-     OU: 'www.digicert.com',
-     CN: 'DigiCert SHA2 High Assurance Server CA' },
-  subjectaltname: 'DNS:*.netsville.com, DNS:netsville.com',
-  infoAccess: 
-   { 'OCSP - URI': [ 'http://ocsp.digicert.com' ],
-     'CA Issuers - URI': [ 'http://cacerts.digicert.com/DigiCertSHA2HighAssuranceServerCA.crt' ] },
-  modulus: 'C7013C205D7..........B3C8765DE4AD9D39',
-  exponent: '10001',
-  valid_from: 'Jun 28 00:00:00 2015 GMT',
-  valid_to: 'Jul 11 12:00:00 2016 GMT',
-  fingerprint: '74:9F:95:D7:A5:4B:3B:06:59:32:2A:97:D1:A0:37:7D:93:A6:F3:02',
-  ext_key_usage: [ '1.3.6.1.5.5.7.3.1', '1.3.6.1.5.5.7.3.2' ],
-  serialNumber: '03CB385EB2961C88233799B4C7439548',
-  raw: <Buffer>,
-  issuerCertificate: 
-   { subject: 
-      { C: 'US',
-        O: 'DigiCert Inc',
-        OU: 'www.digicert.com',
-        CN: 'DigiCert SHA2 High Assurance Server CA' },
-     issuer: 
-      { C: 'US',
-        O: 'DigiCert Inc',
-        OU: 'www.digicert.com',
-        CN: 'DigiCert High Assurance EV Root CA' },
-     infoAccess: { 'OCSP - URI': [Object] },
-     modulus: 'B6E02FC22406C8..................6892532F5EE3',
-     exponent: '10001',
-     valid_from: 'Oct 22 12:00:00 2013 GMT',
-     valid_to: 'Oct 22 12:00:00 2028 GMT',
-     fingerprint: 'A0:31:C4:67:82:E6:E6:C6:62:C2:C8:7C:76:DA:9A:A6:2C:CA:BD:8E',
-     ext_key_usage: [ '1.3.6.1.5.5.7.3.1', '1.3.6.1.5.5.7.3.2' ],
-     serialNumber: '04E1E7A4DC5CF2F36DC02B42B85D159F',
-     raw: <Buffer>,
-     issuerCertificate: 
-      { subject: [Object],
-        issuer: [Object],
-        modulus: 'C6CCE573E6FBD87................5D1A500B2012CC41BB6E0B5138B84BCB',
-        exponent: '10001',
-        valid_from: 'Nov 10 00:00:00 2006 GMT',
-        valid_to: 'Nov 10 00:00:00 2031 GMT',
-        fingerprint: '5F:B7:EE:06:33:E2:59:DB:AD:0C:4C:9A:E6:D3:8F:1A:61:C7:DC:25',
-        serialNumber: '02AC5C266A0B409B8F0B79F2AE462577',
-        raw: <Buffer>,
-        issuerCertificate: [Circular] } } }
-*/
-
     var socket = tls.connect(tlsOptions, function() {
       if (options.verbose || options.debug) console.log('[tlsSocket] connected');
       peerCert = socket.getPeerCertificate(true);
@@ -179,19 +117,20 @@ var Shaaa = {
     socket.on('end', function() {
       if (options.verbose || options.debug) console.log('[tlsSocket] disconnected');
 
-      // process peerCert and all the issuer certs inside it
-      // console.log(peerCert);
-      var certsarray = [];
+      // Walk through peerCert object.  Grab DER-encoded certs.  Convert to PEM and push to certsArray.
+      var certsArray = [];
+      function eachDer(cert) {
+        var pem = Shaaa.derToPem(cert.raw);
+        certsArray.push(x509.parseCert(pem));
+        if (cert.issuerCertificate !== cert)
+          eachDer(cert.issuerCertificate);
+      }
+      eachDer(peerCert);
 
-      Shaaa.derToPem(peerCert.raw, function(err, pem) {
-        var pemCert = x509.parseCert(pem.toString());
-        // console.log(pemCert);
-        certsarray.push(pemCert);
-        if (certsarray.length == 0)
-          callback({message: "No certs returned"});
-        else
-          callback(null, certsarray);
-      });
+      if (certsArray.length == 0)
+        callback({message: "No certs returned"});
+      else
+        callback(null, certsArray);
     });
 
     socket.on('error', function(error) {
@@ -219,8 +158,8 @@ var Shaaa = {
   cert: function(cert) {
     var answer = Shaaa.algorithm(cert.signatureAlgorithm);
 
-    // var root = Shaaa.isRoot(cert);
-    // var replacement = (root ? null : Shaaa.sha2URL(cert.fingerPrint));
+    var root = Shaaa.isRoot(cert);
+    var replacement = (root ? null : Shaaa.sha2URL(cert.fingerPrint));
 
     var root = null;
     var replacement = null;
@@ -229,8 +168,8 @@ var Shaaa = {
       algorithm: answer.algorithm,
       raw: answer.raw,
       good: (root || answer.good),
-//      root: root,
-//      replacement: replacement,
+      root: root,
+      replacement: replacement,
 
       expires: cert.notAfter,
       name: cert.subject.commonName
@@ -297,7 +236,7 @@ var Shaaa = {
 }
 
 // load roots and fingerprints on first require
-// Shaaa.loadRoots();
+Shaaa.loadRoots();
 Shaaa.loadFingerprints();
 
 module.exports = Shaaa;
